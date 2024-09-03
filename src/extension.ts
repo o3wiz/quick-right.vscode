dbfc9de39d7fa07f856e6d2d4c7583045eefab8e
Daniel Aviv <dani6laviv@gmail.com>
2024-09-04 00:18:43 +0300

    implementing openWorkspaceNewWindow & replaceWorkspaceWithFolder

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,25 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "quickright" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('quickright.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from quickright!');
-	});
-
-	context.subscriptions.push(disposable);
+function openWorkspaceNewWindow(uri: vscode.Uri): void {
+	vscode.commands.executeCommand("vscode.openFolder", uri, { forceNewWindow: true });
+}
+function replaceWorkspaceWithFolder(uri: vscode.Uri): void {
+	vscode.commands.executeCommand("vscode.openFolder", uri, { forceReuseWindow: true });
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+async function openFolderFiles(uri: vscode.Uri) {
+}
+
+function openFolderFilesRecursive(uri: vscode.Uri): void {
+}
+
+export function activate(context: vscode.ExtensionContext) {
+	context.subscriptions.push(
+		vscode.commands.registerCommand("quickright.openWorkspaceNewWindow", openWorkspaceNewWindow),
+		vscode.commands.registerCommand("quickright.replaceWorkspaceWithFolder", replaceWorkspaceWithFolder),
+		vscode.commands.registerCommand("quickright.openFolderFiles", openFolderFiles),
+		vscode.commands.registerCommand("quickright.openFolderFilesRecursive", openFolderFilesRecursive),
+	);
+}
+
+export function deactivate() { }
