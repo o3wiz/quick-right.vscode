01fe80a46db32797ee8bfcf1396a8faf99f72336
Daniel Aviv <dani6laviv@gmail.com>
2024-09-04 21:48:00 +0300

    implementing open folder files (regular and recursive)

--- src/extension.ts
+++ src/extension.ts
@@ -3,14 +3,27 @@ import * as vscode from 'vscode';
 function openWorkspaceNewWindow(uri: vscode.Uri): void {
 	vscode.commands.executeCommand("vscode.openFolder", uri, { forceNewWindow: true });
 }
+
 function replaceWorkspaceWithFolder(uri: vscode.Uri): void {
 	vscode.commands.executeCommand("vscode.openFolder", uri, { forceReuseWindow: true });
 }
 
-async function openFolderFiles(uri: vscode.Uri) {
+async function openFileInEditor(file: vscode.Uri) {
+	const document = await vscode.workspace.openTextDocument(file);
+	await vscode.window.showTextDocument(document, { preview: false });
 }
 
-function openFolderFilesRecursive(uri: vscode.Uri): void {
+async function openFiles(filesPattern: vscode.RelativePattern) {
+	const files = await vscode.workspace.findFiles(filesPattern);
+	await Promise.all(files.map(file => openFileInEditor(file)));
+}
+
+async function openFolderFiles(uri: vscode.Uri) {
+	await openFiles(new vscode.RelativePattern(uri.fsPath, "*"));
+}
+
+async function openFolderFilesRecursive(uri: vscode.Uri) {
+	await openFiles(new vscode.RelativePattern(uri.fsPath, "**/*"));
 }
 
 export function activate(context: vscode.ExtensionContext) {
